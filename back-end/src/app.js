b3d8e7597c54a1dd3cff70790ef0ede804c6cc0d
debora <debora.r.serra@gmail.com>
2022-09-20 18:09:27 -0300

    feat: add login route to app

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
 const errorMiddleware = require('./middlewares/error.middleware');
+const loginRoute = require('./routes/Login.route');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/login', loginRoute);
 
 app.use(errorMiddleware);
 
