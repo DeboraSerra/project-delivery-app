700438b2b982ed61b603c36e44ed9a0a32a2efc0
debora <debora.r.serra@gmail.com>
2022-09-20 18:18:52 -0300

    fix: add password encryption to login service

--- back-end/src/services/Login.service.js
+++ back-end/src/services/Login.service.js
@@ -1,4 +1,5 @@
 const model = require('../models/Login.model');
+const bcrypt = require('bcrypt');
 const { validateLogin, validateEmail } = require('../helpers/validations');
 const CodeError = require('../helpers/CodeError');
 
@@ -7,14 +8,19 @@ const LoginService = {
     const value = await validateLogin(info);
     const exists = await model.getUser(value.email);
     if (exists) throw new CodeError('User already exists', 400);
-    const id = await model.createUser(value);
+    const hash = await bcrypt.hash(value.password, 15);
+    const id = await model.createUser({ ...value, password: hash });
     return { ...value, id };
   },
   getUser: async (email, password) => {
     await validateEmail({ email });
     const user = await model.getUser(email);
-    if (!user || user.password !== password) {
+    if (!user) {
       throw CodeError('Invalid e-mail or password', 400);
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      throw new CodeError('Invalid e-mail or password', 400);
     }
     const { password: pass, ...info } = user;
     return info;
