b30474b9dd400563a90da5301778c3087b058cbb
debora <debora.r.serra@gmail.com>
2022-09-21 19:12:57 -0300

    feat: add Product route to app

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,12 +3,14 @@ require('express-async-errors');
 const cors = require('cors');
 const errorMiddleware = require('./middlewares/error.middleware');
 const loginRoute = require('./routes/Login.route');
+const productRoute = require('./routes/Products.route');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/login', loginRoute);
+app.use('/products', productRoute);
 
 app.use(errorMiddleware);
 
