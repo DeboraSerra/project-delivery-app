1858ac3f336d789cd973a1cf238e7f105999a5dc
debora <debora.r.serra@gmail.com>
2022-09-21 18:40:59 -0300

    feat: add Product model

--- back-end/dump.sql
+++ back-end/dump.sql
@@ -125,9 +125,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'Vanessa Luftnagle','vanessa.luft@email.com','$2b$15$V9ectBc9dq3T.ovEl5Qp5eE.1/5KnZL4lCdVCTj5d6mszON/cwEjC','customer'),(3,'administrator','admin@email.com','$2b$15$dWqLSqCrbZt1Fjlde1rqk.iM/BTZnMVPPCzE9hHZmec7RZDNu.hiS','admin'),(4,'seller seller','seller@email.com','$2b$15$01QhB.InznKKyLUwd55U9ety216HzEXjIW6LkpIVbILn3yWeLuh9i','seller');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -139,3 +137,22 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2022-09-20 18:43:07
+
+INSERT INTO users (id, name, email, password, role) VALUES
+  (1, 'Delivery App Admin', 'adm@deliveryapp.com', '$2b$15$dWqLSqCrbZt1Fjlde1rqk.iM/BTZnMVPPCzE9hHZmec7RZDNu.hiS', 'admin'), -- senha: admin_pass
+  (2, 'Fulana Pereira', 'fulana@deliveryapp.com', '$2b$15$01QhB.InznKKyLUwd55U9ety216HzEXjIW6LkpIVbILn3yWeLuh9i', 'seller'), -- senha: seller_pass
+  (3, 'Cliente Zé Birita', 'zebirita@email.com', '$2b$15$V9ectBc9dq3T.ovEl5Qp5eE.1/5KnZL4lCdVCTj5d6mszON/cwEjC', 'customer'); -- senha: vanessinha123
+UNLOCK TABLES;
+
+INSERT INTO products (id, name, price, url_image) VALUES
+	(1, 'Skol Lata 250ml',2.20, 'http://localhost:3001/images/skol_lata_350ml.jpg'),
+	(2, 'Heineken 600ml',7.50, 'http://localhost:3001/images/heineken_600ml.jpg'),
+	(3, 'Antarctica Pilsen 300ml',2.49, 'http://localhost:3001/images/antarctica_pilsen_300ml.jpg'),
+	(4, 'Brahma 600ml',7.50, 'http://localhost:3001/images/brahma_600ml.jpg'),
+	(5, 'Skol 269ml',2.19, 'http://localhost:3001/images/skol_269ml.jpg'),
+	(6, 'Skol Beats Senses 313ml',4.49, 'http://localhost:3001/images/skol_beats_senses_313ml.jpg'),
+	(7, 'Becks 330ml',4.99, 'http://localhost:3001/images/becks_330ml.jpg'),
+	(8, 'Brahma Duplo Malte 350ml',2.79, 'http://localhost:3001/images/brahma_duplo_malte_350ml.jpg'),
+	(9, 'Becks 600ml',8.89, 'http://localhost:3001/images/becks_600ml.jpg'),
+	(10, 'Skol Beats Senses 269ml',3.57, 'http://localhost:3001/images/skol_beats_senses_269ml.jpg'),
+	(11, 'Stella Artois 275ml',3.49, 'http://localhost:3001/images/stella_artois_275ml.jpg');
---
+++ back-end/src/models/Products.model.js
@@ -0,0 +1,41 @@
+const conn = require('./connection');
+
+const ProductsModel = {
+  getAll: async () => {
+    const query = `SELECT * FROM products;`;
+    const [products] = await conn.execute(query);
+    return products;
+  },
+  getOne: async (id) => {
+    const query = `SELECT * FROM products WHERE id = ?;`;
+    const [[product]] = await conn.execute(query, [id]);
+    return product;
+  },
+  create: async ({ name, price, image }) => {
+    const query = `
+      INSERT INTO products (name, price, url_image)
+      VALUES (?, ?, ?);
+    `;
+    const [{ insertId }] = await conn.execute(query, [name, price, image]);
+    return { name, price, image, id: insertId };
+  },
+  updatePrice: async ({ id, price }) => {
+    const query = `
+      UPDATE products
+      SET price = ?
+      WHERE id = ?;
+    `;
+    const [product] = await conn.execute(query, [price, id]);
+    return product;
+  },
+  delete: async (id) => {
+    const query = `
+     DELETE FROM products
+     WHERE id = ?;
+    `;
+    const [product] = await conn.execute(query, [id]);
+    return product;
+  },
+};
+
+module.exports = ProductsModel;
