b32f8923889cf40a93036317cdd9dad1250f38d3
debora <debora.r.serra@gmail.com>
2022-09-21 19:30:18 -0300

    feat: add token middlewares

--- back-end/src/routes/Products.route.js
+++ back-end/src/routes/Products.route.js
@@ -1,15 +1,16 @@
 const controller = require('../controllers/Product.controller');
 const { Router } = require('express');
+const { adminMiddleware, customerMiddleware } = require('../middlewares/Token.middleware');
 
 const router = Router();
 
 router.route('/')
-  .get(controller.getAll)
-  .post(controller.create);
+  .get(customerMiddleware, controller.getAll)
+  .post(adminMiddleware, controller.create);
 
 router.route('/:id')
-  .get(controller.getOne)
-  .patch(controller.updatePrice)
-  .delete(controller.delete);
+  .get(customerMiddleware, controller.getOne)
+  .patch(adminMiddleware, controller.updatePrice)
+  .delete(adminMiddleware, controller.delete);
 
 module.exports = router;
