db8d23eca564cd901c296ac285d74a8dcea01fa9
debora <debora.r.serra@gmail.com>
2022-09-21 19:01:23 -0300

    feat: add Product service

---
+++ back-end/src/services/Products.service.js
@@ -0,0 +1,33 @@
+const model = require('../models/Products.model');
+const CodeError = require('../helpers/CodeError');
+const { validateProduct } = require('../helpers/validations');
+
+const ProductsServices = {
+  getAll: async () => {
+    const products = await model.getAll();
+    return products;
+  },
+  getOne: async (id) => {
+    const product = await model.getOne(id);
+    if (!product) throw new CodeError('Product not found', 404);
+    return product;
+  },
+  create: async (info) => {
+    const data = await validateProduct(info);
+    const product = await model.create(data);
+    return product;
+  },
+  updatePrice: async (id, price) => {
+    if (price < 0.5) throw new CodeError('Price must be greater than 0.5', 400);
+    const exists = await model.getOne(id);
+    if (!exists) throw new CodeError('Product not found', 404);
+    const product = await model.updatePrice({ id, price });
+    return product;
+  },
+  delete: async (id) => {
+    const product = await model.delete(id);
+    return product;
+  },
+};
+
+module.exports = ProductsServices;
