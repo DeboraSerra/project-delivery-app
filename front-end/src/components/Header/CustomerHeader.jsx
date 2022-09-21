b7d3478655420c8708da123737dce7c467064561
gitdoallan <mailtoallan@icloud.com>
2022-09-21 20:15:10 -0300

    feat: top bar setup

--- front-end/src/components/Header/CustomerHeader.jsx
+++ front-end/src/components/Header/CustomerHeader.jsx
@@ -1,55 +1,154 @@
+import { useState } from 'react';
 import {
-  Box, Button, Link, Typography,
+  Box, Toolbar, IconButton, Typography, Menu, Avatar, Button, Tooltip, MenuItem, Container,
 } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
-import { NightModeToggle } from 'components/Buttons';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useTheme } from '@mui/material/styles';
 import { Logo } from 'components/Images';
-import { Container } from 'components/Common';
-import { useSelector } from 'react-redux';
+import { NightModeToggle } from 'components/Buttons';
+
+const pages = ['Products', 'My Orders'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export function CustomerHeader() {
-  const { userInfo } = useSelector((state) => state);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
   const theme = useTheme();
 
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <Container paddingY={{ xs: 1 / 2, sm: 1 }} maxWidth={{ md: '1100px' }}>
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        width="100%"
-      >
-        <Link
-          component={RouterLink}
-          to="/"
-          underline="none"
+    <Container maxWidth="xl">
+      <Toolbar disableGutters>
+        <Typography
+          variant="h6"
+          noWrap
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: theme.palette.text.primary,
+            textDecoration: 'none',
+          }}
         >
           <Logo />
-        </Link>
-        <Box display="flex" alignItems="center">
-          <Box marginRight="1rem">
-            <NightModeToggle />
-          </Box>
-          <Typography variant="subtitle" sx={{ color: theme.palette.text.primary }}>
-            Olá,
-            {' '}
-            {userInfo.name}
-            {' '}
-            !
-          </Typography>
-          <Box marginLeft="1rem">
-            <Button
-              variant="contained"
-              color="primary"
-              component={RouterLink}
-              to="/logout"
-            >
-              Logout
-            </Button>
-          </Box>
+        </Typography>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography textAlign="center" sx={{ color: theme.palette.text.primary }}>{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
         </Box>
-      </Box>
+        <Typography
+          variant="h5"
+          noWrap
+          sx={{
+            mr: 2,
+            display: { xs: 'flex', md: 'none' },
+            flexGrow: 1,
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: theme.palette.text.primary,
+            textDecoration: 'none',
+          }}
+        >
+          <Logo />
+        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{
+                my: 2, textTransform: 'uppercase', fontWeight: 'bold', m: 1, color: theme.palette.text.primary, display: 'block',
+              }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+
+        <Box marginRight="1rem">
+          <NightModeToggle />
+        </Box>
+
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
     </Container>
   );
 }
