1f67937364c308ddd40b5d043ead56d5844de9db
gitdoallan <mailtoallan@icloud.com>
2022-09-21 23:23:02 -0300

    fix: hardcoded url address

--- back-end/dump.sql
+++ back-end/dump.sql
@@ -145,14 +145,14 @@ INSERT INTO users (id, name, email, password, role) VALUES
 UNLOCK TABLES;
 
 INSERT INTO products (id, name, price, url_image) VALUES
-	(1, 'Skol Lata 250ml',2.20, 'http://localhost:3001/images/skol_lata_350ml.jpg'),
-	(2, 'Heineken 600ml',7.50, 'http://localhost:3001/images/heineken_600ml.jpg'),
-	(3, 'Antarctica Pilsen 300ml',2.49, 'http://localhost:3001/images/antarctica_pilsen_300ml.jpg'),
-	(4, 'Brahma 600ml',7.50, 'http://localhost:3001/images/brahma_600ml.jpg'),
-	(5, 'Skol 269ml',2.19, 'http://localhost:3001/images/skol_269ml.jpg'),
-	(6, 'Skol Beats Senses 313ml',4.49, 'http://localhost:3001/images/skol_beats_senses_313ml.jpg'),
-	(7, 'Becks 330ml',4.99, 'http://localhost:3001/images/becks_330ml.jpg'),
-	(8, 'Brahma Duplo Malte 350ml',2.79, 'http://localhost:3001/images/brahma_duplo_malte_350ml.jpg'),
-	(9, 'Becks 600ml',8.89, 'http://localhost:3001/images/becks_600ml.jpg'),
-	(10, 'Skol Beats Senses 269ml',3.57, 'http://localhost:3001/images/skol_beats_senses_269ml.jpg'),
-	(11, 'Stella Artois 275ml',3.49, 'http://localhost:3001/images/stella_artois_275ml.jpg');
+	(1, 'Skol Lata 250ml',2.20, '/images/skol_lata_350ml.jpg'),
+	(2, 'Heineken 600ml',7.50, '/images/heineken_600ml.jpg'),
+	(3, 'Antarctica Pilsen 300ml',2.49, '/images/antarctica_pilsen_300ml.jpg'),
+	(4, 'Brahma 600ml',7.50, '/images/brahma_600ml.jpg'),
+	(5, 'Skol 269ml',2.19, '/images/skol_269ml.jpg'),
+	(6, 'Skol Beats Senses 313ml',4.49, '/images/skol_beats_senses_313ml.jpg'),
+	(7, 'Becks 330ml',4.99, '/images/becks_330ml.jpg'),
+	(8, 'Brahma Duplo Malte 350ml',2.79, '/images/brahma_duplo_malte_350ml.jpg'),
+	(9, 'Becks 600ml',8.89, '/images/becks_600ml.jpg'),
+	(10, 'Skol Beats Senses 269ml',3.57, '/images/skol_beats_senses_269ml.jpg'),
+	(11, 'Stella Artois 275ml',3.49, '/images/stella_artois_275ml.jpg');
