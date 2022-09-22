cad72075d0f31e4becb5a59e492ec84671856785
gitdoallan <mailtoallan@icloud.com>
2022-09-22 14:32:45 -0300

    feat: checkout setup

--- front-end/src/components/Buttons/ShoppingCartFloating.jsx
+++ front-end/src/components/Buttons/ShoppingCartFloating.jsx
@@ -1,14 +1,17 @@
 import { Box, Fab } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export function ShoppingCartFloating() {
   const { cartItems } = useSelector((state) => state);
+  const navigate = useNavigate();
   const totalItems = cartItems.reduce((acc, item) => acc + item.qty, 0);
   const totalPrice = cartItems.reduce((acc, item) => acc + item.qty * item.productPrice, 0);
   return (
     <Box sx={{ '& > :not(style)': { m: 1 } }}>
       <Fab
+        onClick={() => navigate('/checkout')}
         color="secondary"
         variant="extended"
         aria-label="add"
---
+++ front-end/src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,15 @@
+import { Header } from 'components/Header';
+import { Footer } from 'components/Footer';
+import { Container } from 'components/Common';
+
+export function Checkout() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Checkout</h1>
+      </Container>
+      <Footer />
+    </>
+  );
+}
